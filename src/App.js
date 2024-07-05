b6397a86598773b2735ef0c95ed7cbb4fb761f20
akshaydev42 <akshay283746@gmail.com>
2024-07-05 16:20:20 +0530

    commit in main branch

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Weather from './Components/Weather';
+
+function App() {
+  return (
+    <div className="App">
+      <h2 className = "heading">Enter valid City Name for Weather Report</h2>
+    <Weather></Weather>
+    </div>
+  );
+}
+
+export default App;
